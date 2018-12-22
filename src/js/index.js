8cb1bdd9e715de61864e970be1eaca74f9af598a
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-12-22 17:03:00 +0000

    building the search controller

--- src/js/index.js
+++ src/js/index.js
@@ -5,18 +5,21 @@
 //console.log(`Using imported functions ${searchViews.add(searchViews.ID,2)} and ${searchViews.multiply(3,5)}.${str}`)
 //624a6b312a1abed6cf2bd597b50709fa
 //https://www.food2fork.com/api/search
-import axios from 'axios';
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '624a6b312a1abed6cf2bd597b50709fa';
-    try {
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log('recipes :', recipes);
-        console.log('res :', res);
-    } catch (error) {
-        alert(error);
+import Search from './models/Search';
+
+const state = {
+
+};
+const controlSearch = async() => {
+    const query = "pizza"
+    if (query) {
+        state.search = new Search(query);
+        await state.search.getResults();
+        console.log('state.search.results :', state.search.results);
     }
 
 }
-getResults('pizza');
+document.querySelector(
+    ".search").addEventListener("submit", e => {
+    e.preventDefault();
+})
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'i am an exported string';
+import axios from 'axios';
+export default class Search {
+    constructor(query) {
+        this.query = query;
+
+
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '624a6b312a1abed6cf2bd597b50709fa';
+        try {
+            const res = await axios.get(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            //  axios.get('https://cors-anywhere.herokuapp.com/' + yourUrl)
+            this.result = res.data.recipes;
+            console.log('recipes :', this.result);
+        } catch (error) {
+            alert(error);
+        }
+
+    }
+}
