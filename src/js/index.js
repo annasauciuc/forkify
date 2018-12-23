b1a0aece3de2f8bfbc5cc63ee36af91a8b54909a
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-12-23 23:51:40 +0000

    refactoring search comtroler

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,49 @@
-// Global app controller
-//import str from './models/Search';
-//import { add as a, multiply as m, ID } from './views/searchView';
-//import * as searchViews from './views/searchView'
-//console.log(`Using imported functions ${searchViews.add(searchViews.ID,2)} and ${searchViews.multiply(3,5)}.${str}`)
-//624a6b312a1abed6cf2bd597b50709fa
-//https://www.food2fork.com/api/search
 import Search from './models/Search';
 import Recipe from './models/Recipe';
-import * as searchViews from './views/searchViews';
+import List from './models/List';
+import Likes from './models/Likes';
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
-const state = {
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-};
+/** 
+ * SEARCH CONTROLLER
+ */
 const controlSearch = async() => {
-    const query = searchViews.getInput();
-    console.log('query :', query);
+    // 1) Get query from view
+    const query = searchView.getInput();
+
     if (query) {
+        // 2) New search object and add to state
         state.search = new Search(query);
-        searchViews.clearInputs();
-        searchViews.clearResults();
-        renderLoader(elements.searchRes)
-        await state.search.getResults();
-        clearLoader();
-        searchViews.renderResults(state.search.result);
 
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        try {
+            // 4) Search for recipes
+            await state.search.getResults();
+
+            // 5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
     }
-
 }
 elements.searchForm.addEventListener("submit", e => {
     e.preventDefault();
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,29 +1,31 @@
 export const elements = {
-    searchForm: document.querySelector(".search"),
+    searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list'),
-    searchRes: document.querySelector(".results"),
-    searchResPages: document.querySelector(".results__pages"),
+    searchResPages: document.querySelector('.results__pages'),
     recipe: document.querySelector('.recipe'),
     shopping: document.querySelector('.shopping__list'),
     likesMenu: document.querySelector('.likes__field'),
     likesList: document.querySelector('.likes__list')
-
 };
-export const elementsStrings = {
+
+export const elementStrings = {
     loader: 'loader'
 };
+
 export const renderLoader = parent => {
-    const loader = ` <div class="${elementsStrings.loader}">
-    <svg>
-       <use href="img/icons.svg#icon-cw"></use>
-    </svg>
-    </div>`;
-    parent.insertAdjacentHTML("afterbegin", loader);
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
 };
+
 export const clearLoader = () => {
-    const loader = document.querySelector(`.${elementsStrings.loader}`);
-    if (loader) {
-        loader.parentElement.removeChild(loader);
-    }
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if (loader) loader.parentElement.removeChild(loader);
 };
