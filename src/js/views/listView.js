ea89f8af4733fec49e47e514daf360ec31088234
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-12-26 21:46:55 +0000

    refactoring code

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -1,34 +1,24 @@
 import { elements } from './base';
-import { limitRecipeTitle } from './searchView';
 
-export const toggleLikeBtn = isLiked => {
-    const iconString = isLiked ? 'icon-heart' : 'icon-heart-outlined';
-    document.querySelector('.recipe__love use').setAttribute('href', `img/icons.svg#${iconString}`);
-    // icons.svg#icon-heart-outlined
-};
-
-export const toggleLikeMenu = numLikes => {
-    elements.likesMenu.style.visibility = numLikes > 0 ? 'visible' : 'hidden';
-};
-
-export const renderLike = like => {
+export const renderItem = item => {
     const markup = `
-        <li>
-            <a class="likes__link" href="#${like.id}">
-                <figure class="likes__fig">
-                    <img src="${like.img}" alt="${like.title}">
-                </figure>
-                <div class="likes__data">
-                    <h4 class="likes__name">${limitRecipeTitle(like.title)}</h4>
-                    <p class="likes__author">${like.author}</p>
-                </div>
-            </a>
+        <li class="shopping__item" data-itemid=${item.id}>
+            <div class="shopping__count">
+                <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
+                <p>${item.unit}</p>
+            </div>
+            <p class="shopping__description">${item.ingredient}</p>
+            <button class="shopping__delete btn-tiny">
+                <svg>
+                    <use href="img/icons.svg#icon-circle-with-cross"></use>
+                </svg>
+            </button>
         </li>
     `;
-    elements.likesList.insertAdjacentHTML('beforeend', markup);
+    elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
 
-export const deleteLike = id => {
-    const el = document.querySelector(`.likes__link[href*="${id}"]`).parentElement;
-    if (el) el.parentElement.removeChild(el);
-}
+export const deleteItem = id => {
+    const item = document.querySelector(`[data-itemid="${id}"]`);
+    if (item) item.parentElement.removeChild(item);
+};
