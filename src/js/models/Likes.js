e9a2a7bee2adda29bf02ea0133cf327af60cfb5b
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-12-26 20:36:59 +0000

    adding the likes model

---
+++ src/js/models/Likes.js
@@ -0,0 +1,14 @@
+export default class Likes {
+    constructor() {
+        this.likes = [];
+    }
+
+    addLike(id, title, author, img) {
+        const like = { id, title, author, img };
+        this.likes.push(like);
+
+        // Perist data in localStorage
+        this.persistData();
+
+        return like;
+    }
