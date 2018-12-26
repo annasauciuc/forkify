6282ac9181d7cf5ae01d34ce99892517d1e6c047
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-12-26 19:46:37 +0000

    highliting the selected element

--- src/js/views/searchViews.js
+++ src/js/views/searchViews.js
@@ -1,10 +1,48 @@
 import { elements } from './base';
+
 export const getInput = () => elements.searchInput.value;
-export const clearInputs = () => { elements.searchInput.value = ""; };
+
+export const clearInput = () => {
+    elements.searchInput.value = '';
+};
+
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
     elements.searchResPages.innerHTML = '';
+};
+
+export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    });
+    document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
+};
+
+/*
+// 'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
+*/
+export const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
 }
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -22,30 +60,16 @@ const renderRecipe = recipe => {
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
+// type: 'prev' or 'next'
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
 
-
-export const limitRecipeTitle = (title, limit = 17) => {
-    const newTitle = [];
-    if (title.length > limit) {
-        title.split(' ').reduce((acc, cur) => {
-            if (acc + cur.length <= limit) {
-                newTitle.push(cur);
-            }
-            return acc + cur.length;
-        }, 0);
-
-        // return the result
-        return `${newTitle.join(' ')} ...`;
-    }
-    return title;
-};
-const createButton = (page, type) => ` <button class="btn-inline results__btn--${type}" data-goto= ${type==='prev'?page -1:page +1}>
-<svg class="search__icon">
-    <use href="img/icons.svg#icon-triangle-${type === 'prev'?'left':'right'}"></use>
-</svg>
-<span>Page ${type==='prev'?page -1:page +1}</span>
-</button>
-<button class="btn-inline results__btn--next">`;
 const renderButtons = (page, numResults, resPerPage) => {
     const pages = Math.ceil(numResults / resPerPage);
 
